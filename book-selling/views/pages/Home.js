d7a7b4584630ff6b8cf506163727ebb36c11f319
ducquang2 <nducquangg@gmail.com>
2023-12-10 00:29:51 +0700

    feat: add Home screen
    - need to find the design

---
+++ book-selling/views/pages/Home.js
@@ -0,0 +1,22 @@
+import { Navbar } from '../components/index.js'
+
+const Home = {
+  data() {
+    return {};
+  },
+  components: {
+    Navbar,
+  },
+  methods() { },
+  mounted() { },
+  template:
+    `
+      <div>
+        <Navbar />
+        <div>aaaa</div>
+      </div>
+    `
+
+};
+
+export { Home };
