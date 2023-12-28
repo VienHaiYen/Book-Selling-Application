b5f0ae15d69ba09b24780f44d086bd37c279520f
20120634 <94401753+lmtri2307@users.noreply.github.com>
2023-12-28 20:02:32 +0700

    [authen-with-google] login/register with google account

--- book-selling/controllers/auth.c.js
+++ book-selling/controllers/auth.c.js
@@ -3,6 +3,21 @@ const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 const tokenOption = require("../configs/tokenOption")
 const cookieOption = require("../configs/cookieOption")
+const googleOption = require("../configs/googleOption")
+const { user } = require("../configs/connectStr")
+
+class GoogleUser {
+    constructor({ id, email, verified_email, name, given_name, family_name, picture, locale, }) {
+        this.id = id
+        this.email = email
+        this.verified_email = verified_email
+        this.name = name
+        this.given_name = given_name
+        this.family_name = family_name
+        this.picture = picture
+        this.locale = locale
+    }
+}
 
 module.exports = {
     signIn: async (req, res, next) => {
@@ -27,10 +42,15 @@ module.exports = {
                 throw new Error("User does not exist")
             }
 
+            if(userDb.google_id){
+                throw new Error("User is registerd by google")
+            }
+
             const checkResult = await bcrypt.compare(password, userDb.password_hash)
             if (!checkResult) {
                 throw new Error("Password incorrect")
             }
+
             const accessToken = jwt.sign(
                 { email: userDb.email },
                 process.env.ACCESS_TOKEN_SECRET,
@@ -50,5 +70,71 @@ module.exports = {
         } catch (error) {
             next(error)
         }
+    },
+
+    googleLogin: async (req, res, next) => {
+        try {
+            res.redirect(`${googleOption.ggLoginUrl}?${googleOption.query.toString()}`)
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    googleLoginCallback: async (req, res, next) => {
+        try {
+            // get code
+            const code = req.query.code
+            // get access token
+            const authData = await (await fetch(
+                googleOption.ggAuthApiUrl,
+                {
+                    method: 'post',
+                    headers: { 'Content-Type': "application/json" },
+                    body: JSON.stringify(googleOption.getGGAuthApiOption(code))
+                }
+            )).json()
+            console.log(authData)
+            // get client info from google
+            const clientInfo = new GoogleUser(await (await fetch(
+                googleOption.ggInfoApiUrl,
+                {
+                    method: 'get',
+                    headers: { "Authorization": `Bearer ${authData.access_token}` }
+                }
+            )).json())
+            console.log(clientInfo)
+            // get or create new user
+            let user = await User.getByEmail(clientInfo.email)
+            if(!user){
+                user = new User({
+                    email: clientInfo.email,
+                    full_name: clientInfo.name,
+                    google_id: clientInfo.id,
+                })
+                user = await User.create(user)
+            }
+            // set token
+            const accessToken = jwt.sign(
+                { email: user.email },
+                process.env.ACCESS_TOKEN_SECRET,
+                tokenOption.accessToken
+            )
+            res.cookie('aToken', accessToken, cookieOption)
+            res.redirect("/")
+        } catch (error) {
+            next(error)
+        }
     }
 }
+
+// response from google
+// {
+//     id: '114180075313625028899',
+//     email: 'haiyenvien2307@gmail.com',
+//     verified_email: true,
+//     name: 'Hải Yến Viên',
+//     given_name: 'Hải Yến',
+//     family_name: 'Viên',
+//     picture: 'https://lh3.googleusercontent.com/a/ACg8ocIQzpTqJ9mOY3ywaDqy4TS14GJx3Gn6fMCrQHBMOw8Z=s96-c',
+//     locale: 'vi'
+//   }
