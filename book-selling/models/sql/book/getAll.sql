cc412d5f344f00f042b0126dab18b5ebb6ce1307
ducquang2 <nducquangg@gmail.com>
2024-01-22 14:25:21 +0700

    feat: add sort to getAllBooks

--- book-selling/models/sql/book/getAll.sql
+++ book-selling/models/sql/book/getAll.sql
@@ -1,5 +1,5 @@
 SELECT books.*, COUNT(*) OVER(), book_inventory.available_quantity, book_inventory.unit_price
 FROM public."books"
 LEFT JOIN book_inventory ON id = book_inventory.book_id
-ORDER BY id
+ORDER BY $3^ $4^ NULLS LAST
 LIMIT $1 OFFSET $2
