c6aa6221b789a9b1ef74418c6de0ff96a483cc95
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-01-27 16:31:34 +0700

    book shop call balance api from payment

--- book-selling/routers/auth.r.js
+++ book-selling/routers/auth.r.js
@@ -1,5 +1,6 @@
 const { authController } = require("../controllers");
 const { verifyUser } = require("../middlewares/verifyUser");
+const { User } = require("../models");
 
 const router = require("express").Router();
 
@@ -9,6 +10,7 @@ router.get("/login/google", authController.googleLogin)
 router.get("/login/google/callback", authController.googleLoginCallback)
 router.post("/logout", authController.logout)
 router.get("/auth", verifyUser(), authController.getAuth)
+router.get("/balance", verifyUser(User.roles.admin), authController.getBalance)
 
 // router.get("/logined", verifyUser(), (req, res, next) => { res.send("logined") })
 // router.get("/admin", verifyUser(User.roles.admin), (req, res, next) => { res.send("admin") })
